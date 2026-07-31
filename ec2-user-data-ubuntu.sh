8eea37dcc3fff1e25082ce9a1693915fc6c01bcf
Inga Kaydanova <inga.kaidanova@jobleads.com>
2026-07-31 20:06:29 +0300

    Changed dockerhub user name

--- ec2-user-data-ubuntu.sh
+++ ec2-user-data-ubuntu.sh
@@ -5,11 +5,11 @@ apt-get update
 apt-get install -y docker.io
 systemctl enable --now docker
 
-docker pull DOCKERHUB_USERNAME/plant-tracker:1.0
+docker pull kaydanova/plant-tracker:1.0
 docker volume create plant-data
 docker run -d \
   --name plant-tracker \
   --restart unless-stopped \
   -p 2468:80 \
   -v plant-data:/data \
-  DOCKERHUB_USERNAME/plant-tracker:1.0
+  kaydanova/plant-tracker:1.0
